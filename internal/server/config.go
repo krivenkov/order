85065d524984fb796c8a69feb2296a526f994d1a
Vasily Krivenkov <v.krivenkov@greatbook.ru>
2024-04-09 17:05:35 +0300

    add grpc server in fx

--- internal/server/config.go
+++ internal/server/config.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"github.com/krivenkov/order/internal/server/grpc"
 	"github.com/krivenkov/order/internal/server/http"
 	"go.uber.org/fx"
 )
@@ -9,4 +10,5 @@ type Config struct {
 	fx.Out
 
 	HTTP http.Config `json:"http" yaml:"http" envPrefix:"HTTP_"`
+	GRPC grpc.Config `json:"grpc" yaml:"grpc" envPrefix:"GRPC_"`
 }
--- internal/server/fx.go
+++ internal/server/fx.go
@@ -1,10 +1,12 @@
 package server
 
 import (
+	"github.com/krivenkov/order/internal/server/grpc"
 	"github.com/krivenkov/order/internal/server/http"
 	"go.uber.org/fx"
 )
 
 var FXModule = fx.Options(
 	http.FXModule,
+	grpc.FXModule,
 )
